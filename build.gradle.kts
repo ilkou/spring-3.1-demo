8b5d1aaf0b78c849a4ee926854a277cd4273af3a
o.ouklich <o.ouklich@cirestechnologies.ma>
2023-05-22 11:50:09 +0100

    add observability

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 //	compileOnly("org.projectlombok:lombok")
 //	developmentOnly("org.springframework.boot:spring-boot-devtools")
 //	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
--- src/main/java/com/ouklich/demo/productivity/ProductivityApplication.java
+++ src/main/java/com/ouklich/demo/productivity/ProductivityApplication.java
@@ -1,15 +1,26 @@
 package com.ouklich.demo.productivity;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.annotation.Observed;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 public class ProductivityApplication {
+
+	@Bean
+	public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+		return new ObservedAspect(observationRegistry);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductivityApplication.class, args);
@@ -31,6 +42,7 @@ class CustomerController {
 	}
 
 
+	@Observed(name = "getCustomers")
 	@GetMapping("/customers")
 	Iterable<Customer> getCustomers() {
 		return customerRepository.findAll();
